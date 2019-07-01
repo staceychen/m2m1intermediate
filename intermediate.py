3cb12c30b3fef662a46188104e9fa11cf4e46d8a
RA Account <raaccount@shdh-dhcp-113-115.wharton.upenn.edu>
2019-07-01 16:34:49 -0400

    accounted remote clusters that are n/a

--- intermediate.py
+++ intermediate.py
@@ -13,30 +13,37 @@ try:
         #create the list of ungrouped addresses
         for row in reader:
             airport_radius = row['airport_radius']
-    with open('inputs/input_test.csv', encoding='utf\\-8-sig') as csvfile: 
+    with open('inputs/input.csv', encoding='utf-8-sig') as csvfile: 
             reader = csv.DictReader(csvfile, delimiter=',')
             
             for row in reader:
-                cluster_list = []
-                patent_id = row['patent_id']
-                # local cluster
-                local_dict = ast.literal_eval(row['local_cluster'])
-                local_lat = local_dict['center_lat']
-                local_lon = local_dict['center_lng']
-                is_local = True
-                local_cluster = Group.group(is_local, patent_id, airport_radius, local_lat, local_lon)
-                cluster_list.append(local_cluster)
-                
-                # remote clusters
-                remote_dict_list = ast.literal_eval(row['remote_cluster'])
-           
-                for remote_dict in remote_dict_list:
-                    remote_lat = remote_dict['center_lat']
-                    remote_lon = remote_dict['center_lng']
-                    is_local = False
-                    remote_cluster = Group.group(is_local, patent_id, airport_radius, local_lat, local_lon)
-                    cluster_list.append(remote_cluster)
-                all_cluster_lists.append(cluster_list)
+                print(reader.line_num)
+                if row['remote_cluster'] == 'N/A':
+                    continue
+                else:
+                    cluster_list = []
+                    patent_id = row['patent_id']
+                    # local cluster
+                    local_dict = ast.literal_eval(row['local_cluster'])
+                    local_lat = local_dict['center_lat']
+                    local_lon = local_dict['center_lng']
+            
+                    is_local = True
+                    local_cluster = Group.group(is_local, patent_id, airport_radius, local_lat, local_lon)
+                    cluster_list.append(local_cluster)
+                    
+                    # remote clusters
+                    
+                    remote_dict_list = ast.literal_eval(row['remote_cluster'])
+               
+                    for remote_dict in remote_dict_list:
+                        remote_lat = remote_dict['center_lat']
+                        remote_lon = remote_dict['center_lng']
+                        is_local = False
+                        remote_cluster = Group.group(is_local, patent_id, airport_radius, remote_lat, remote_lon)
+                     
+                        cluster_list.append(remote_cluster)
+                    all_cluster_lists.append(cluster_list)
 except:
     # when no arguments updated for arguments.csv
     try:
